d91c3fd62e8cc34af4785552a0a59fd44c052c32
Foreygon <111753615+Foreygon@users.noreply.github.com>
2022-11-02 20:08:36 +0100

    first commit

---
+++ menu.js
@@ -0,0 +1,32 @@
+// Variables
+const header = document.getElementById('header');
+// const menu_list = document.getElementById('menu_list');
+// const hamburger = document.getElementById('hamburger-logo');
+// const menu_div = document.getElementById('menu-div');
+
+const scroll_id = document.getElementById('scroll_id');
+const nav_switch_color = document.getElementById('nav_switch_color');
+const switch_color_black = document.getElementsByClassName('switch_color_black');
+const switch_color_light = document.getElementsByClassName('switch_color_light');
+
+console.log(scroll_id);
+console.log(nav_switch_color);
+console.log(switch_color_black);
+console.log(switch_color_light);
+
+
+// hamburger.addEventListener('click', ()=>{
+//     if(menu_div.classList.contains('menu-hide')){
+//         menu_div.classList.remove('menu-hide');
+//         menu_div.classList.add('menu-show');
+//     }else{
+//         menu_div.classList.add('menu-hide');
+//         menu_div.classList.remove('menu-show');
+//     }
+// })
+
+// hamburger.addEventListener('scroll', ()=>{
+//     if 
+// }
+
+// )
